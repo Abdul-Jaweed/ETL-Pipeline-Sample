0488c3d43c61185108c55cb3838b2b500148452c
Abdul Jaweed <114526738+Abdul-Jaweed@users.noreply.github.com>
2023-08-22 20:26:10 +0000

    updating Data Extraction code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     print("Data Extaction Started")
     data_processor = DataDownloaderExtractor()
     data_processor.download_and_extract()
+    extracted_data = data_processor.extract()
+    print(extracted_data)
+
 
 if __name__ == "__main__":
     main()
--- src/data_extractor.py
+++ src/data_extractor.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import zipfile
+import pandas as pd
+import glob
+import xml.etree.ElementTree as ET
 from dotenv import load_dotenv
 
 class DataDownloaderExtractor:
@@ -33,10 +36,45 @@ class DataDownloaderExtractor:
         else:
             print("Data already extracted.")
     
+    def extract_from_csv(self, file_to_process):
+        return pd.read_csv(file_to_process)
+    
+    def extract_from_json(self, file_to_process):
+        return pd.read_json(file_to_process, lines=True)
+    
+    def extract_from_xml(self, file_to_process):
+        data = []
+        tree = ET.parse(file_to_process)
+        root = tree.getroot()
+        for person in root:
+            name = person.find("name").text
+            height = float(person.find("height").text)
+            weight = float(person.find("weight").text)
+            data.append({"name": name, "height": height, "weight": weight})
+        return pd.DataFrame(data)
+    
+    def extract(self):
+        extracted_data = pd.DataFrame(columns=['name', 'height', 'weight'])
+        
+        for csvfile in glob.glob(os.path.join(self.extracted_dir, "*.csv")):
+            extracted_data = pd.concat([extracted_data, self.extract_from_csv(csvfile)], ignore_index=True)
+
+        for jsonfile in glob.glob(os.path.join(self.extracted_dir, "*.json")):
+            extracted_data = pd.concat([extracted_data, self.extract_from_json(jsonfile)], ignore_index=True)
+
+        for xmlfile in glob.glob(os.path.join(self.extracted_dir, "*.xml")):
+            extracted_data = pd.concat([extracted_data, self.extract_from_xml(xmlfile)], ignore_index=True)
+
+        return extracted_data
+    
     def download_and_extract(self):
         self.download_data()
         self.extract_data()
         print("Data download and extraction completed.")
 
+
+
 # data_processor = DataDownloaderExtractor()
 # data_processor.download_and_extract()
+# extracted_data = data_processor.extract()
+# print(extracted_data)
