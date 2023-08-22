10cd3a82e3b7806709a25b77b3a5e5f8621e4d0c
Abdul Jaweed <114526738+Abdul-Jaweed@users.noreply.github.com>
2023-08-22 20:18:09 +0000

    organizing code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from data_extractor import DataDownloaderExtractor
+from src.data_extractor import DataDownloaderExtractor
 
 def main():
+
+    print("Data Extaction Started")
     data_processor = DataDownloaderExtractor()
     data_processor.download_and_extract()
 
